72d2bc70b79db1280d88bc1a0ff2f92cdc4ea3da
saicj <saicj@ubint.jp>
2016-04-12 16:02:56 +0900

    Update # Shop form Done

--- shop_form_done.php
+++ shop_form_done.php
@@ -133,14 +133,14 @@ session_regenerate_id(true);
     $honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
     mb_language('Japanese');
     mb_internal_encoding('UTF-8');
-    mb_send_mail($email,$title,$honbun,$header);
+    // mb_send_mail($email,$title,$honbun,$header);
 
     $title='お客様からご注文がありました。';
-    $header='From:'.$email;
-    $honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
-    mb_language('Japanese');
-    mb_internal_encoding('UTF-8');
-    mb_send_mail('info@rokumarunouen.co.jp',$title,$honbun,$header);
+    // $header='From:'.$email;
+    // $honbun=html_entity_decode($honbun,ENT_QUOTES,'UTF-8');
+    // mb_language('Japanese');
+    // mb_internal_encoding('UTF-8');
+    // mb_send_mail('info@rokumarunouen.co.jp',$title,$honbun,$header);
 
   }
   catch (Exception $e)
