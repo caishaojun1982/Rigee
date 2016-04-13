2a1905297d1af7150da0669dfc62e8ccc79a9fcc
saicj <saicj@ubint.jp>
2016-04-13 13:33:59 +0900

    Update # cart look fix

--- shop_cartlook.php
+++ shop_cartlook.php
@@ -3,7 +3,7 @@
 session_start ();
 session_regenerate_id (true);
 
-if (isset ($SESSION['member_login']) == false) {
+if (isset ($_SESSION['member_login']) == false) {
 
   print 'ようこそゲスト様　';
   print '<a href="member_login.html">会員ログイン</a><br />';
